eb3c319148e4d8a557417483f24da070948040d6
Kresten <kresten@mamu.dk>
2018-10-08 16:12:48 +0200

    added password hashing

--- fullstack_vue_graphql/client/src/components/Home.vue
+++ fullstack_vue_graphql/client/src/components/Home.vue
@@ -1,26 +1,44 @@
 <template>
   <v-container text-xs-center>
-    <!--
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indeterminate :size="70" :width="7" color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
     <v-flex xs12>
-      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
-        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+      <v-carousel v-if="!loading && posts.length > 0" v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
       </v-carousel>
     </v-flex>
-    -->
-
 
   </v-container>
 </template>
 
 <script>
-import { gql } from 'apollo-boost';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'home',
   created() {
     this.handleGetCarouselPosts();
+  },
+  computed: { 
+    ...mapGetters(['loading', 'posts'])
+    /*
+    To avoid all these getters in our components, we can use the more concise vuex mapGetters as above
+    posts() {
+      return this.$store.getters.posts;
+    },
+    loading() {
+      return this.$store.getters.loading;
+    }
+    */
   },
   methods: {
     handleGetCarouselPosts() {
--- fullstack_vue_graphql/client/src/store.js
+++ fullstack_vue_graphql/client/src/store.js
@@ -1,36 +1,46 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { gql } from 'apollo-boost';
 import { defaultClient as apolloClient } from './main';
+
+import { GET_POSTS } from './queries';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    posts: [],
+    loading: false
   },
   mutations: {
-
+    setPosts: (state, payload) => {
+      state.posts = payload;
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
   },
   actions: {
-    getPosts: () => {
+    getPosts: ({ commit }) => {
+      commit('setLoading', true);
       // use ApolloClient to fire getPosts query
       apolloClient
         .query({
-          query: gql`
-          query {
-            getPosts {
-              _id
-              title
-              imageUrl
-            }
-          }`
-        }).then(data => {
-          console.log(data);
-        }).catch(err => {
+          query: GET_POSTS
+        })
+        .then(({ data }) => {
+          // commit allows us to pass data from an an action to a mutation function
+          commit('setPosts', data.getPosts);
+          commit('setLoading', false);
+        })
+        .catch(err => {
+          commit('setLoading', false);
           console.error(err);
         });
     }
+  },
+  getters: {
+    posts: state => state.posts,
+    loading: state => state.loading
   }
 });
